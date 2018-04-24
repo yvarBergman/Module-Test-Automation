07a56f2b8cbdac7c25354d4bf72b5fec9551d031
Yvar Bergman <yvarbergman@gmail.com>
2018-04-24 14:34:11 +0200

    Answer for exercise 4 + 5 added

--- tests/exercise_4.js
+++ tests/exercise_4.js
@@ -1,8 +1,12 @@
 module.exports = {
   '@tags' : ['exercise_4'],
-  'Exercise 4' : function(browser) {
-    browser.url()
-
+  'search' : function(browser) {
+    browser.url('https://www.calco.nl')
+           .click('body > div.pageWrapper > header > div.bottomBar.container > nav > ul > li.level-1.search > a > span')
+           .setValue('.js-searchInputModal','traineeship')
+           .click('#search > button')
+           .assert.urlEquals('https://www.calco.nl/zoeken?q=traineeship') 
+           .assert.containsText('body > div.pageWrapper > section.hero.sub > div > div > div > div > div', '12 resultaten voor traineeship')
            .end();
   }
 }
