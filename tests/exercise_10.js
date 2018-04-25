31abfd784c734d90faa81b7776a280a29912a8f4
Yvar Bergman <yvarbergman@gmail.com>
2018-04-25 09:37:21 +0200

    Added exercise 10

--- tests/exercise_10.js
+++ tests/exercise_10.js
@@ -1,8 +1,9 @@
 module.exports = {
   '@tags' : ['exercise_10'],
   'Exercise 10' : function(browser) {
-    browser.url()
-
+    browser.url('https://www.calco.nl')
+           .waitForElementVisible('#topLogo', 1000)
+           .assert.screenshotIdenticalToBaseline('#topLogo', 'logo')
            .end();
   }
 }
